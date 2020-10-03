860685f66a7d3a9684fc7ce91faed8b74f671986
nicledomaS <kovalenko_juri@mail.ru>
2020-10-04 00:30:10 +0300

    Update clang_tidy.cmake

--- check_codes/clang_tidy.cmake
+++ check_codes/clang_tidy.cmake
@@ -11,12 +11,12 @@ option(ENABLE_TIDY "Enable clang-tidy" OFF)
 function(clang_tidy_analysis TARGET)
     if(ENABLE_TIDY AND CLANG_TIDY)
         message(STATUS "clang-tidy is found")
+        
+        set(CHECKS "")
         set_target_properties(${TARGET}
                 PROPERTIES CXX_CLANG_TIDY
-                "${CLANG_TIDY};-checks=${OPTIONS_CHECKS};-warnings-as-errors=*;-p=${CMAKE_BINARY_DIR};-header-filter='${CMAKE_CURRENT_SOURCE_DIR}/*'")
+                "${CLANG_TIDY};-checks=${CHECKS};-warnings-as-errors=*;-p=${CMAKE_BINARY_DIR};-header-filter='${CMAKE_CURRENT_SOURCE_DIR}/*'")
     else()
         message(STATUS "clang-tidy is not found or not enable")
     endif()
-
-
 endfunction(clang_tidy_analysis)
